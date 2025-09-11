90b554552704687ace6e8be9f1ba6a07e3ad5775
dopajac <154491661+dopajac@users.noreply.github.com>
2025-09-11 15:32:59 +0900

    총기 제작중

---
+++ TPS/Assets/Script/Weapon/Weapon.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private float damage;
+    private float bulletSpeed;
+    private float fire_rate;
+    private bool isSingle_engine;
+    private bool max_bullet_clip;
+    private bool cur_bullet_clip;
+
+    private void Shot()
+    {
+        
+    }
+
+    private void ReLoad()
+    {
+        
+    }
+    
+    
+}
